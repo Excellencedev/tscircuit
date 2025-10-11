395bd255625a5c6a91dea0f84ee3de08958885fc
Excellencedev <ademiluyisuccessandexcellence@gmail.com>
2025-10-11 06:01:43 +0100

    fix: render reference designators

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export * from "@tscircuit/core"
 export * from "@tscircuit/eval"
 export type { ChipProps, PinLabelsProp, CommonLayoutProps } from "@tscircuit/props"
+export * from "./scripts/patch-circuit-json-to-gltf"
